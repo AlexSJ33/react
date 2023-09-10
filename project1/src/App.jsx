944ba4772124d8c1f94fdcc41faaebfbf5e66a9f
AlexSJ <alevildark@gmail.com>
2023-09-10 20:36:17 -0300

    TodoList

---
+++ project1/src/App.jsx
@@ -0,0 +1,12 @@
+import Titulo from "./Titulo.jsx";
+
+export default function App() {
+  return(
+    <>
+    <Titulo cor= "red"/>
+    <Titulo cor= "blue"/>
+    <Titulo cor= "green"/>
+    <Titulo cor= "brown"/>
+    </>
+  )
+}
---
+++ todo-list/src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TodoList() {
+  return(
+
+    <h1 className="
+    bg-zinc-600"
+    >Hello World</h1>
+  )
+}
+ 
